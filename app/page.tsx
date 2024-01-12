62fe7ec6bc5188ecd06adf9e8c7b9df5f8b20bfd
zkPeep <epiqgames@proton.me>
2024-01-12 16:43:21 -0600

    update README

--- app/page.tsx
+++ app/page.tsx
@@ -98,13 +98,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Built at{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            ETH GLOBAL
           </p>
         </a>
       </div>
